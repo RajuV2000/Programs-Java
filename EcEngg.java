90159bc3970f243a97eeb9a01fbc2c75d275ae29
Raju V <89912771+RajuV2000@users.noreply.github.com>
2021-10-28 16:16:28 +0530

    Delete EcEngg.java

--- EcEngg.java
+++
@@ -1,7 +0,0 @@
-class EcEngg extends Engineer
-{
- public void display()
- {
-  System.out.println("\nName: " + name + "\nBranch: " + branch + "\nCompany: " + company + "\nSalary: " + salary + "\nMobile Number: " + mblNum);
- }
-}
